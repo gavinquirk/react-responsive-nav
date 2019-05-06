b4872ce5ca0faf82dd56ffbf52baeb96f09927d6
Gavin Quirk <gavinquirk@gmail.com>
2019-05-06 14:56:44 -0700

    basic top nav setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+
+import Toolbar from './components/Toolbar/Toolbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Responsive Nav Bar</h1>
+      <Toolbar />
+      <main>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
+          eaque eligendi distinctio dolor in sequi dignissimos? Eius perferendis
+          eos perspiciatis nobis sed repellendus delectus aspernatur quidem
+          consectetur! Cupiditate consequatur molestias labore ducimus quos
+          neque laborum velit, aperiam, exercitationem, aut dolorem repellat
+          praesentium. Iste dolore asperiores, nisi aperiam excepturi quaerat
+          impedit.
+        </p>
+      </main>
     </div>
   );
 }
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import DrawerToggleButton from './SideDrawer/DrawerToggleButton';
+import './Toolbar.css';
+
+const toolbar = props => (
+  <header className='Toolbar'>
+    <nav className='Toolbar__navigation'>
+      <div>
+        <DrawerToggleButton />
+      </div>
+      <div className='Toolbar__logo'>
+        <a href='#'>THE LOGO</a>
+      </div>
+      <div className='spacer' />
+      <div className='Toolbar__navigation-items'>
+        <ul>
+          <li>
+            <a href='#'>Link 1</a>
+          </li>
+          <li>
+            <a href='#'>Link 2</a>
+          </li>
+          <li>
+            <a href='#'>Link 3</a>
+          </li>
+          <li>
+            <a href='#'>Link 4</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default toolbar;
